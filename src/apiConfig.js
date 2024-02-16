4b87f2cdea43c0a9d8de92a9b4b927215602925d
CHL <Clundquist1@gmail.com>
2024-02-16 09:45:12 -0500

    apiConfig fix

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: '<https://apimanyfaces.fly.dev>',
+	production: 'https://apimanyfaces.fly.dev',
 	development: 'http://localhost:8000',
 }
 
